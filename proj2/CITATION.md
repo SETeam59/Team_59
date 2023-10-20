74dd9be0361210d33b5d42e0e314cabbec237017
SETeam59 <145417903+SETeam59@users.noreply.github.com>
2023-10-19 23:55:05 -0400

    Delete proj2 directory

--- proj2/CITATION.md
+++
@@ -1,3 +0,0 @@
-# ZENODO DOI BADGE
-
-[![DOI](https://zenodo.org/badge/417325535.svg)](https://zenodo.org/badge/latestdoi/417325535) 
--- proj2/shutdown.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-kill $(ps | grep npm | awk '{print $1}')
-kill $(ps | grep node | awk '{print $1}')
-kill $(ps | grep flask | awk '{print $1}')
